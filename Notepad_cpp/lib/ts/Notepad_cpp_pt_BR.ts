da8a1867d9d3970a67494a1bdf04ed1ada13d382
lucasfturos <lucasfturos@gmail.com>
2023-03-19 18:31:00 -0300

    Init notepad in C++

---
+++ Notepad_cpp/lib/ts/Notepad_cpp_pt_BR.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build-Notepad_cpp-Desktop-Debug/Notepad_cpp_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
