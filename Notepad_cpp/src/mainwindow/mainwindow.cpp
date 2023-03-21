940085e38212a44ac9ba0d41954b5f901f024fe0
lucasfturos <lucasfturos@gmail.com>
2023-03-21 10:49:09 -0300

    Notepad cpp - Aba file pronta

--- Notepad_cpp/src/mainwindow/mainwindow.cpp
+++ Notepad_cpp/src/mainwindow/mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    move(screen()->geometry().center() - frameGeometry().center());
+    this->setCentralWidget(ui->textEdit);
+    setWindowTitle("Sem Título - Bloco de anotações");
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,87 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionNovo_triggered()
+{
+    current_file.clear();
+    setWindowTitle("Sem Título - Bloco de anotações");
+    ui->textEdit->setText(QString());
+}
+
+void MainWindow::on_actionAbrir_triggered()
+{
+    QString file_name = QFileDialog::getOpenFileName();
+    QFile file(file_name);
+    current_file = file_name;
+
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Aviso: ", "Não foi possível abrir o arquivo " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(file_name + " - Bloco de anotações");
+
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSalvar_triggered()
+{
+    QString file_name = QFileDialog::getSaveFileName();
+    QFile file(file_name);
+    current_file = file_name;
+
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Aviso: ", "Não foi possível salvar o arquivo " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(file_name + " - Bloco de anotações");
+
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionSair_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionCopiar_triggered()
+{
+
+}
+
+void MainWindow::on_actionColar_triggered()
+{
+
+}
+
+void MainWindow::on_actionRecortar_triggered()
+{
+
+}
+
+void MainWindow::on_actionDesfazer_triggered()
+{
+
+}
+
+void MainWindow::on_actionRefazer_triggered()
+{
+
+}
+
+void MainWindow::on_actionSobre_o_aplicativo_triggered()
+{
+    QMessageBox message_about;
+
+    message_about.setWindowTitle("Sobre");
+    message_about.setText("<h3 align='center'><pre>\tFeito por Lucas Turos\t\t\nFoi feito em Qt5 e C++</pre></h3>");
+    message_about.exec();
+}
--- Notepad_cpp/src/mainwindow/mainwindow.hpp
+++ Notepad_cpp/src/mainwindow/mainwindow.hpp
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include <QFile>
+#include <QScreen>
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+   void on_actionNovo_triggered();
+   void on_actionAbrir_triggered();
+   void on_actionSalvar_triggered();
+
+   void on_actionSair_triggered();
+
+   void on_actionCopiar_triggered();
+
+   void on_actionColar_triggered();
+
+   void on_actionRecortar_triggered();
+
+   void on_actionDesfazer_triggered();
+
+   void on_actionRefazer_triggered();
+
+   void on_actionSobre_o_aplicativo_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    QString current_file{};
 };
 #endif // MAINWINDOW_HPP
